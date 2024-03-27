50fecea0fd92f5f7e622d0378041e56d2b94aaaf
rased <rased100@gmail.com>
2024-03-27 12:58:48 +0600

    managementtwo design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Features from "./pages/Features/Features";
 import ManagementOne from "./pages/Management/ManagementOne";
+import ManagementTwo from "./pages/Management/ManagementTwo";
 import Banner from "./pages/banner/Banner";
 import { NavbarDefault } from "./pages/navbar/Navbar";
 
@@ -11,6 +12,7 @@ function App() {
       <Banner />
       <Features />
       <ManagementOne />
+      <ManagementTwo />
     </div>
   );
 }
--- src/pages/Management/ManagementTwo.js
+++ src/pages/Management/ManagementTwo.js
@@ -1,22 +1,24 @@
 import React from "react";
-import chartimg from "../../assets/images/manage1/Image1.png";
+import chartimg from "../../assets/images/manage1/Image2.png";
 import "./mOne.css";
+import { GrShieldSecurity } from "react-icons/gr";
+import { HiSpeakerphone } from "react-icons/hi";
 
-const ManagementOne = () => {
+const ManagementTwo = () => {
   return (
-    <div className="manageone-container">
+    <div className="managetwo-container">
       <div className="container mx-auto py-20">
         <div className="flex flex-col lg:flex-row items-center">
           {/* first half of the screen */}
-          <div className="lg:flex-1 order-2 lg:order-1 lg:mr-0 mr-[7%]">
+          <div className="lg:flex-1 order-2 lg:mr-0 mr-[7%]">
             <img src={chartimg} alt="" className="mt-10" />
           </div>
 
-          <div className="lg:flex-1 lg:ml-5 mt-5 lg:mt-0 order-1 lg:order-2">
+          <div className="lg:flex-1 lg:ml-5 mt-5 lg:mt-0 order-1 ">
             {/* second half of the screen */}
             <div className="mb-10">
-              <h1 className="text-3xl font-bold">
-                Allocate Leads and Simplify Lead Management
+              <h1 className="text-3xl font-bold w-[70%]">
+                Ways A Project Management Tool Can Help Your Team
               </h1>
               <p className="text-lg mt-5">
                 Unique and powerful suite of software to run your entire
@@ -24,8 +26,10 @@ const ManagementOne = () => {
                 to transform the way you work.
               </p>
             </div>
-            <div className="flex flex-col my-5 lg:border lg:rounded-3xl lg:p-5">
-              <span className="h-[21px] w-[21px] bg-[#47CFFF] rounded-full pl-5 ml-5 mr-5"></span>
+            <div className=" my-5 lg:border lg:rounded-3xl lg:p-5">
+              <span className="">
+                <GrShieldSecurity size={30} color="#49E4EB" />
+              </span>
               <div>
                 <h2 className="text-xl font-semibold">Lead generation</h2>
                 <p className="text-md mt-2">
@@ -34,8 +38,10 @@ const ManagementOne = () => {
                 </p>
               </div>
             </div>
-            <div className="flex flex-col my-5 lg:border lg:rounded-3xl lg:p-5">
-              <span className="h-[21px] w-[21px] bg-[#47CFFF] rounded-full pl-5 ml-5 mr-5"></span>
+            <div className=" my-5 lg:border lg:rounded-3xl lg:p-5">
+              <span className="">
+                <HiSpeakerphone size={30} color="#49E4EB" />
+              </span>
               <div>
                 <h2 className="text-xl font-semibold">Team Email</h2>
                 <p className="text-md mt-2">
@@ -51,4 +57,4 @@ const ManagementOne = () => {
   );
 };
 
-export default ManagementOne;
+export default ManagementTwo;
